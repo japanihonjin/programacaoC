a499244683c62871d8bbd6e4ff3faa544cd586b3
japanihonjin <74779619+japanihonjin@users.noreply.github.com>
2026-03-30 08:50:41 -0300

    Update n2.codar.c

--- n2.codar.c
+++ n2.codar.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main(){
+  int idade, matricula;
+  float altura;
+  char nome{50};
+
+  printf("Digite sua idade: \n);
+  scanf("%d, &idade);
+
+  printf("Digite sua altura: \n);
+  scanf("%f, &altura);
+
+  printf("Digite seu nome: \n);
+  scanf("%s, &nome);
+
+  printf("Digite sua matrícula: \n);
+  scanf("%d, &matricula);
+
+  printf("Nome do aluno: %s - Matrícula: %d", nome, matrícula);
+  printf("Idade: %d - Altura: %f", idade, altura);
+
+  return 0;
+
+    
+}
